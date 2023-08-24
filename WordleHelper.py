5ea662fa8f727bf62f883e1a2827b5bc6138e98c
Max Moundas <maxmoundas@Maxs-MacBook-Pro-2.local>
2023-08-24 18:14:43 -0500

    input validation added

--- WordleHelper.py
+++ WordleHelper.py
@@ -12,13 +12,24 @@ def load_words():
     # Filter out words to only include 5-letter words
     return [word for word in word_list if len(word) == 5]
 
+def valid_exclusion_indices(input_str):
+    """Check if the input string for exclusion indices is valid"""
+    valid_nums = ["1", "2", "3", "4", "5"]
+    nums = input_str.split(",")
+    return all(num.strip() in valid_nums for num in nums) and len(nums) == len(set(nums))
+
 def get_exclusions(chars):
     exclusions = {}
     for char in chars:
-        exclusion_indices = input(f"Enter indices (1-5) where '{char}' should NOT appear (comma-separated): ")
-        # Convert indices to 0-based indexing
-        exclusion_indices = [int(i)-1 for i in exclusion_indices.split(",") if i.isdigit()]
-        exclusions[char] = exclusion_indices
+        while True:
+            exclusion_indices = input(f"Enter indices (1-5) where '{char}' should NOT appear (comma-separated): ")
+            if valid_exclusion_indices(exclusion_indices):
+                # Convert indices to 0-based indexing
+                exclusion_indices = [int(i)-1 for i in exclusion_indices.split(",") if i.isdigit()]
+                exclusions[char] = exclusion_indices
+                break
+            else:
+                print("Invalid input. Please try again.")
     return exclusions
 
 def possible_words(exclusions, pattern, forbidden):
@@ -56,12 +67,33 @@ def possible_words(exclusions, pattern, forbidden):
 
     return possible
 
+def valid_unplaced_chars(input_str):
+    """Check if the input string for unplaced characters is valid"""
+    return all(char.isalpha() for char in input_str)
+
+def valid_pattern(input_str):
+    """Check if the input string for word pattern is valid"""
+    return len(input_str) == 5 and all(char.isalpha() or char == '_' for char in input_str)
+
+def valid_forbidden_chars(input_str):
+    """Check if the input string for forbidden characters is valid"""
+    return all(char.isalpha() for char in input_str)
+
+def get_valid_input(prompt, validation_func):
+    """Get valid input using the provided prompt and validation function"""
+    while True:
+        user_input = input(prompt)
+        if validation_func(user_input):
+            return user_input
+        else:
+            print("Invalid input. Please try again.")
+
 def main():
     while True:
-        unplaced_chars = input("Enter characters that are in the word but placement is unknown: ")
+        unplaced_chars = get_valid_input("Enter characters that are in the word but placement is unknown: ", valid_unplaced_chars)
         exclusions = get_exclusions(unplaced_chars)
-        pattern = input("Enter the word pattern with known placements (use '_' for unknown spots): ")
-        forbidden = input("Enter the characters that are NOT in the word: ")
+        pattern = get_valid_input("Enter the word pattern with known placements (use '_' for unknown spots): ", valid_pattern)
+        forbidden = get_valid_input("Enter the characters that are NOT in the word: ", valid_forbidden_chars)
 
         words = possible_words(exclusions, pattern, forbidden)
 
@@ -71,5 +103,5 @@ def main():
         if cont.lower() != "yes":
             break
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
